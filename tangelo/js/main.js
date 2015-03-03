c7c3624accfe9a17ecd1d1246f29e4fa7a369cb0
Jeffrey Baumes <jeff.baumes@kitware.com>
2015-03-03 15:02:16 -0500

    restructuring ui to have navbar

--- tangelo/js/main.js
+++ tangelo/js/main.js
@@ -74,53 +74,113 @@ $(function () {
         ]
     };
 
-    d3.json('image-knn', function (error, result) {
-        console.log(result);
-        $('.imgspace-image-knn').nodelink({
-            data: result,
-            width: $('.imgspace-image-knn').width(),
-            height: $('.imgspace-image-knn').height()
-        });
-    });
-
-    d3.json('solr-pivot', function (error, result) {
-        var data = [];
-        result.forEach(function (d1) {
-            d1.pivot.forEach(function (d2) {
-                data.push({
-                    width: +d1.value,
-                    height: +d2.value,
-                    count: d2.count
+    function textSearch() {
+        var searchBox = $('<input type="text" class="form-control input-lg" placeholder="Search">');
+        $('.image-space-search-heading').text("Search by image text")
+        $('.image-space-search-content').empty().append(searchBox);
+        searchBox.keypress(function (event) {
+            if (event.which === 13) {
+                $('.search-progress').removeClass('hidden');
+                d3.json('solr-query?query=' + encodeURIComponent(searchBox.val()), function (error, data) {
+                    $('.image-space-search-results').empty();
+                    function imageUrl(d) {
+                        var parts = d.id.split('/'),
+                            file = parts[parts.length - 1]
+                        return 'https://s3.amazonaws.com/roxyimages/' + file;
+                    }
+                    var results = d3.select('.image-space-search-results').selectAll('div')
+                        .data(data)
+                        .enter().append('div')
+                        .attr('class', 'col-xs-6 col-md-3');
+                    results.append('a')
+                        .attr('href', imageUrl)
+                        .attr('target', '_blank')
+                        .attr('class', 'thumbnail')
+                        .append('img')
+                        .attr('class', 'blur')
+                        .attr('src', imageUrl)
+                    results.append('pre').text(function (d) { return d.content[0].trim(); });
+                    $('.search-progress').addClass('hidden');
                 });
+            }
+        });
+    }
+
+    function similarityGraph() {
+        var graph = $('<div style="height:400px;width:100%"></div>');
+        $('.search-ui-progress').removeClass('hidden');
+        $('.image-space-search-heading').text("Browse image similarity graph")
+        $('.image-space-search-content').empty().append(graph);
+        d3.json('image-knn', function (error, result) {
+            console.log(result);
+            graph.nodelink({
+                data: result,
+                width: 400,
+                height: 400
             });
+            $('.search-ui-progress').addClass('hidden');
         });
-        console.log(data);
-        vg.parse.spec(scatterplot, function(chart) {
-            chart({
-                el:".imgspace-sizes",
-                data: {
-                    table: data
-                }
-            }).update();
-        });
-    });
+    }
 
-    // d3.json('similarity', function (error, result) {
-    //     console.log(data);
-    // });
-
-    $('.imgspace-text-search').keypress(function (event) {
-        if (event.which === 13) {
-            d3.json('solr-query/' + encodeURIComponent($('.imgspace-text-search').val()), function (error, data) {
-                d3.select('.imgspace-text-search-results').selectAll('div').remove();
-                var results = d3.select('.imgspace-text-search-results').selectAll('div').data(data).enter().append('div');
-                results.append('pre').text(function (d) { return d.content[0].trim(); });
-                results.append('img')
-                    // .attr('class', 'blur')
-                    .attr('src', function (d) { return 'https://s3.amazonaws.com/roxyimages/' + d.Filename; });
-                console.log(data);
+    var sizeData;
+    function sizeFilter() {
+        function displaySizeChart() {
+            vg.parse.spec(scatterplot, function(chart) {
+                var vis = chart({
+                    el: visContainer[0],
+                    data: {
+                        table: sizeData
+                    }
+                }).on("click", function(event, item) {
+                    console.log(item);
+                }).update();
             });
         }
+
+        var visContainer = $('<div></div>');
+        $('.image-space-search-heading').text("Filter by image size")
+        $('.image-space-search-content').empty().append(visContainer);
+
+        if (!sizeData) {
+            $('.search-ui-progress').removeClass('hidden');
+            d3.json('solr-pivot', function (error, result) {
+                sizeData = [];
+                result.forEach(function (d1) {
+                    d1.pivot.forEach(function (d2) {
+                        sizeData.push({
+                            width: +d1.value,
+                            height: +d2.value,
+                            count: d2.count
+                        });
+                    });
+                });
+                $('.search-ui-progress').addClass('hidden');
+                displaySizeChart();
+            });
+        } else {
+            displaySizeChart();
+        }
+    }
+
+    $('.image-space-nav-size').click(function () {
+        $('.navbar-nav li').removeClass('active');
+        $('.image-space-nav-size').parent().addClass('active');
+        sizeFilter();
+        return false;
+    });
+
+    $('.image-space-nav-text').click(function () {
+        $('.navbar-nav li').removeClass('active');
+        $('.image-space-nav-text').parent().addClass('active');
+        textSearch();
+        return false;
+    });
+
+    $('.image-space-nav-graph').click(function () {
+        $('.navbar-nav li').removeClass('active');
+        $('.image-space-nav-graph').parent().addClass('active');
+        similarityGraph();
+        return false;
     });
 
 });
