469f4c7832c99eee6f7c067ce14b0c296934c9f9
Jeffrey Baumes <jeff.baumes@kitware.com>
2015-07-17 11:04:13 -0400

    Removing flask and old tangelo apps
    Fixes #53. Fixes #54.

--- flask/scripts/gunicorn_cfg.py
+++
@@ -1,6 +0,0 @@
-bind='0.0.0.0:8000'
-pidfile='gunicorn_pid'
-accesslog='gunicorn_access.log'
-errorlog='gunicorn_error.log'
-workers=4
-worker_class='sync'
--- tangelo/similarity.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-with open('/code/memex/data/color_2003.p') as dataFile:
-    data = pickle.load(dataFile)
-
-def run():
-    return data
