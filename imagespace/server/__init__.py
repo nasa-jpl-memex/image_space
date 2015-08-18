332c4cfd53b71c432f2fcb09849e2bb6a3e92fab
Jeffrey Baumes <jeff.baumes@kitware.com>
2015-08-17 22:46:20 -0400

    Add google analytics

--- imagespace/server/__init__.py
+++ imagespace/server/__init__.py
@@ -72,6 +72,16 @@ class CustomAppRoot(object):
         <div id="g-global-info-apiroot" class="hide">${apiRoot}</div>
         <div id="g-global-info-staticroot" class="hide">${staticRoot}</div>
 
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-66442136-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+
         <script src="${staticRoot}/built/libs.min.js"></script>
         <script src="${staticRoot}/built/app.min.js"></script>
         <script src="${staticRoot}/built/plugins/gravatar/plugin.min.js">
