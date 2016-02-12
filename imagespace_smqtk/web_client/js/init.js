8110eab77236892632d2a2336759fd235d39ec45
Dan LaManna <dan.lamanna@kitware.com>
2016-02-12 16:33:39 -0500

    Implement client side near duplicate filtering
    This adds a checkbox to the controls panel of SMQTK searches which
    allows the user to filter their searches to only show "near duplicates"
    which are determined by SMQTK plugin threshold of any image with a
    distance <= -1500.

--- imagespace_smqtk/web_client/js/init.js
+++ imagespace_smqtk/web_client/js/init.js
@@ -19,4 +19,49 @@ girder.events.once('im:appload.after', function () {
         { niceName: 'Similarity (SMQTK)' },
         { altUrl: 'smqtk_similaritysearch' }
     );
+
+    /**
+     * Render the search view, and then for smqtk-similarity searches render
+     * the nearDuplicates template in the control bar.
+     **/
+    girder.wrap(imagespace.views.SearchView, 'render', function (render) {
+        render.call(this);
+
+        if (this.mode === 'smqtk-similarity') {
+            this.$('#im-classification-narrow').after(girder.templates.nearDuplicates({
+                near_duplicates: (_.has(this.collection.params, 'near_duplicates') &&
+                                  this.collection.params.near_duplicates === 1)
+            }));
+        }
+    });
+
+    /**
+     * Update the collection when the near duplicates checkbox is acted on.
+     **/
+    imagespace.views.SearchView.prototype.events['change #smqtk-near-duplicates input'] = function (event) {
+        this.collection.params.near_duplicates = Number(event.currentTarget.checked);
+
+        imagespace.updateQueryParams({
+            near_duplicates: this.collection.params.near_duplicates
+        });
+
+        $('.alert-info').html('Narrowing results <i class="icon-spin5 animate-spin"></i>').removeClass('hidden');
+        this.collection.fetch(this.collection.params, true);
+    };
+
+    /**
+     * Handle collection initialize so it accounts for the new query parameter
+     * we're adding (near_duplicates).
+     **/
+    girder.wrap(imagespace.collections.ImageCollection, 'initialize', function (initialize, models, options) {
+        initialize.call(this, models, options);
+
+        if (this.filterByQueryString) {
+            var qs = imagespace.parseQueryString();
+
+            if (_.has(qs, 'near_duplicates')) {
+                this.params.near_duplicates = Number(qs.near_duplicates);
+            }
+        }
+    });
 }, this);
