d6cf74d5c604dbb331864e9eca726d5868d2a255
Dan LaManna <dan.lamanna@kitware.com>
2016-05-16 13:48:08 -0400

    There may be no ads associated with images (Weapons specific)

--- imagespace/server/imagesearch_rest.py
+++ imagespace/server/imagesearch_rest.py
@@ -59,11 +59,17 @@ class ImageSearch(Resource):
                 'docs': []
             }
 
-        response = {
-            'numFound': result['response']['numFound'],
-            'docs': result['response']['docs'],
-            'groupedDocs': []
-        }
+        try:
+            response = {
+                'numFound': result['response']['numFound'],
+                'docs': result['response']['docs'],
+                'groupedDocs': []
+            }
+        except KeyError:
+            return {
+                'numFound': 0,
+                'docs': []
+            }
 
         for (domain, documents) in group_documents_by_domain(response['docs']):
             response['groupedDocs'].append([domain, list(documents)])
