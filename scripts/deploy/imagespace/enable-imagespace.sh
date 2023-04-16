2a40b0cabef25c84cd0749ae44d8831721cbd5fa
Chris Mattmann <chris.mattmann@gmail.com>
2023-04-16 12:30:10 -0700

    Docker container naming conventions have changed.

--- scripts/deploy/imagespace/enable-imagespace.sh
+++ scripts/deploy/imagespace/enable-imagespace.sh
@@ -1,5 +1,5 @@
-docker exec -it deploy_imagespace-mongo_1 mongo girder --eval 'db.setting.update({key: "core.plugins_enabled"}, {$push: {value: "imagespace_smqtk"}})'
-docker exec -it deploy_imagespace-mongo_1 mongo girder --eval 'db.setting.insert({key: "IMAGE_SPACE_SMQTK_NNSS_URL", value: "http://smqtk-services:12345"})'
-docker exec -it deploy_imagespace-mongo_1 mongo girder --eval 'db.setting.insert({key: "IMAGE_SPACE_SMQTK_IQR_URL", value: "http://smqtk-services:12346"})'
-docker exec -it deploy_imagespace-mongo_1 mongo girder --eval 'db.setting.insert({key: "IMAGE_SPACE_DEFAULT_SIMILARITY_SEARCH", value: "smqtk-similarity"})'
-docker exec -it deploy_imagespace-girder_1 touch /girder/girder/conf/girder.dist.cfg
+docker exec -it deploy-imagespace-mongo-1 mongo girder --eval 'db.setting.update({key: "core.plugins_enabled"}, {$push: {value: "imagespace_smqtk"}})'
+docker exec -it deploy-imagespace-mongo-1 mongo girder --eval 'db.setting.insert({key: "IMAGE_SPACE_SMQTK_NNSS_URL", value: "http://smqtk-services:12345"})'
+docker exec -it deploy-imagespace-mongo-1 mongo girder --eval 'db.setting.insert({key: "IMAGE_SPACE_SMQTK_IQR_URL", value: "http://smqtk-services:12346"})'
+docker exec -it deploy-imagespace-mongo-1 mongo girder --eval 'db.setting.insert({key: "IMAGE_SPACE_DEFAULT_SIMILARITY_SEARCH", value: "smqtk-similarity"})'
+docker exec -it deploy-imagespace-girder-1 touch /girder/girder/conf/girder.dist.cfg
